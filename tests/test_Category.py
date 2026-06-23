bf15222aeb4b7e46904159c420d44a7e16860d58
dosed <e.nmitenin@gmail.com>
2026-06-23 19:43:59 +0300

    test: add test for category count

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -1,4 +1,15 @@
+from src.Category import Category
+
 def test_category_initialization(vegetable):
     assert vegetable.name == "Овощи"
     assert vegetable.description == "Свежие овощи"
     assert vegetable.products == ["Огурец", "Помидор"]
+
+def test_category_count():
+    Category.category_count = 0
+    Category.product_count = 0
+
+    Category('Овощи', 'Свежие овощи', [])
+    Category('Фрукты', 'Свежие фрукты', [])
+
+    assert Category.category_count == 2
