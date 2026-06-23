f901e229def9c3558fcd90a2affe50fe9122ceca
dosed <e.nmitenin@gmail.com>
2026-06-23 21:07:45 +0300

    feat: add JSON data loader

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import json
+
+from src.Category import Category
+from src.Product import Product
+
+
+def load_data(
+        path: str,
+) -> list[Category]:
+    with open(
+            path,
+            "r",
+            encoding='UTF-8'
+    ) as f:
+        data = json.load(f)
+
+    categories = []
+
+    for category_data in data:
+        products = []
+
+        for product_data in category_data["products"]:
+            product = Product(
+                name=product_data["name"],
+                description=product_data["description"],
+                price=product_data["price"],
+                quantity=product_data["quantity"],
+            )
+
+            products.append(product)
+
+        category = Category(
+            name=category_data["name"],
+            description=category_data["description"],
+            products=products,
+        )
+        categories.append(category)
+
+    return categories
